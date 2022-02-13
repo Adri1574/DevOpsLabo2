cd75594e3eb774bdd858a78dd6779f453039d92d
Wouter Peetermans <wouter@wouterpeetermans.com>
2022-02-13 23:26:17 +0100

    Fix typo

--- program.sh
+++ program.sh
@@ -3,7 +3,11 @@
 echo "Hello World!";
 echo "Feature 2";
 read -p "Give your name: " name;
+<<<<<<< HEAD
 echo $name;
 echo "Adrian Moniakowski"
 echo "Hello $name. Greetings to you my friend";
 echo "Hello"
+=======
+echo "Hello $name. Greetings to you my friend.";
+echo "fix typo"
